127537c6e2deabdb8ab55f1d74ca0685bb6fb8ea
Elliot Marsden <elliot.marsden@gmail.com>
2015-08-16 19:34:00 +0100

    Fix PyPI classifiers

--- setup.py
+++ setup.py
@@ -44,8 +44,9 @@ setup(
     zip_safe=False,
     keywords='cellulist',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
         "Programming Language :: Python :: 2",
